40b6d941f1b51200c971c4ac9e4a1de70f0fc2f9
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-03-06 09:12:05 +0100

    Add class Prodotto

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,11 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        Prodotto prodotto = new Prodotto("Pallone", "Pallone in cuoio da calcio", new BigDecimal("10.50"), "22%");
+
+       prodotto.stampaDettagli();
+    }
+}
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,40 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Prodotto {
 
+    // Variabili di istanza
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public BigDecimal prezzo;
+    public String iva;
+
+    // Costruttore
+    public Prodotto(String nome, String descrizione, BigDecimal prezzo, String iva) {
+
+        // Genera un codice casuale
+        this.codice = (int) (Math.random() * 999);
+
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    // Metodo per stampare dettagli del prodotto
+    public void stampaDettagli() {
+        System.out.println("Codice: " + codice);
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrizione: " + descrizione);
+        System.out.println("Prezzo: " + prezzo);
+        System.out.println("IVA: " + iva);
+    }
+
+    // Metodo per avere il prezzo base
+
+    // Metodo per calcolare prezzo con IVA
+
+    // Metodo per avere nome esteso (codice-nome)
 }
